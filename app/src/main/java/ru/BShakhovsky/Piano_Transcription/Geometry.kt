d3a881d3d4d488a7fd3d0c012d9732c16a6d5049
Boris Shakhovskiy <BShakhovskey@rambler.ru>
2020-01-07 17:24:55 +0300

    Three lights as in Microsoft DirectXTK default mode

--- app/src/main/java/ru/BShakhovsky/Piano_Transcription/Geometry.kt
+++ app/src/main/java/ru/BShakhovsky/Piano_Transcription/Geometry.kt
@@ -2,63 +2,40 @@
 package ru.BShakhovsky.Piano_Transcription
 
 import android.opengl.GLES31
+import android.opengl.GLException
 import android.opengl.Matrix
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.nio.FloatBuffer
 
 class Geometry {
 
-    private val program = GLES31.glCreateProgram()
-
     companion object {
-        const val whiteLen = 145f
-        const val whiteWid = 23f
+        const val whiteLen   = 145f
+        const val whiteWid   = 23f
         const val overallLen = whiteWid * 52
     }
-    private val whiteGap = .2f
-    private val blackLen = 85f
-    private val blackWid = 9
-    private val blackFillet = 3.5f
+    private val whiteGap    = .6f
+    private val blackLen    = 85f
+    private val blackWid    = 9
+    private val blackFillet = 3
 
-    private fun allocCords(cords : FloatArray) = ByteBuffer.allocateDirect(
-        cords.size * 4).run {
-            order(ByteOrder.nativeOrder())
-            asFloatBuffer().apply {
-                put(cords)
-                position(0)
-            }
-        }
-    private fun allocOrder(orderBuff : ShortArray) = ByteBuffer.allocateDirect(
-        orderBuff.size * 2).run {
-        order(ByteOrder.nativeOrder())
-        asShortBuffer().apply {
-            put(orderBuff)
-            position(0)
-        }
-    }
+    private val whiteLeft  : Primitive
+    private val whiteMid   : Primitive
+    private val whiteRight : Primitive
+    private val black      = Primitive(floatArrayOf(
+        whiteWid - blackWid / 2              , whiteWid + blackWid, 0f,
+        whiteWid + blackWid / 2              , whiteWid + blackWid, 0f,
+        whiteWid - blackWid / 2              , whiteWid + blackWid, blackLen - 3 * blackFillet,
+        whiteWid + blackWid / 2              , whiteWid + blackWid, blackLen - 3 * blackFillet,
+        whiteWid - blackWid / 2 - blackFillet, whiteWid - blackWid, blackLen,
+        whiteWid + blackWid / 2 + blackFillet, whiteWid - blackWid, blackLen,
+        whiteWid - blackWid / 2 - blackFillet, whiteWid - blackWid, 0f,
+        whiteWid + blackWid / 2 + blackFillet, whiteWid - blackWid, 0f
+    ), intArrayOf(0, 2, 1, 1, 2, 3, 2, 4, 3, 3, 4, 5, 0, 6, 2, 2, 6, 4, 1, 3, 7, 7, 3, 5))
 
-    private val whiteLeft  : FloatBuffer
-    private val whiteMid   : FloatBuffer
-    private val whiteRight : FloatBuffer
-    private val blackCords = allocCords(floatArrayOf(
-        whiteWid - blackWid / 2              , whiteWid + blackWid / 2, 0f,
-        whiteWid + blackWid / 2              , whiteWid + blackWid / 2, 0f,
-        whiteWid - blackWid / 2              , whiteWid + blackWid / 2, blackLen - 2 * blackFillet,
-        whiteWid + blackWid / 2              , whiteWid + blackWid / 2, blackLen - 2 * blackFillet,
-        whiteWid - blackWid / 2 - blackFillet, whiteWid - blackWid / 2, blackLen,
-        whiteWid + blackWid / 2 + blackFillet, whiteWid - blackWid / 2, blackLen,
-        whiteWid - blackWid / 2 - blackFillet, whiteWid - blackWid / 2, 0f,
-        whiteWid + blackWid / 2 + blackFillet, whiteWid - blackWid / 2, 0f
-    ))
-    private val whiteOrder = allocOrder(shortArrayOf(
-        0, 2, 1, 1, 2, 3, 4, 6, 5, 5, 6, 7, 6, 8, 7, 7, 8, 9,
-        0, 10, 2, 2, 10, 12, 4, 14, 6, 6, 14, 8, 1, 3, 11, 11, 3, 13, 5, 7, 15, 15, 7, 9))
-    private val blackOrder = allocOrder(shortArrayOf(
-        0, 2, 1, 1, 2, 3, 2, 4, 3, 3, 4, 5, 0, 6, 2, 2, 6, 4, 1, 3, 7, 7, 3, 5))
-
-    private val pos : Int
-    private val color : Int
+    private val pos    : Int
+    private val color  : Int
+    private val norm   : Int
+    private val mv     : Int
+    private val mvp    : Int
 
     init {
         floatArrayOf(  blackWid / 2f + blackFillet, whiteWid, 0f,
@@ -79,91 +56,118 @@ class Geometry {
             whiteWid - blackWid / 2  - blackFillet, 0f      , blackLen,
             whiteGap                              , 0f      , blackLen,
             whiteWid - whiteGap                   , 0f      , blackLen
-        ).also { whiteMidCords ->
-            whiteMid = allocCords(whiteMidCords)
+        ).also { whiteMidCords -> intArrayOf(0, 2, 1, 1, 2, 3, 4, 6, 5, 5, 6, 7, 6, 8, 7, 7, 8, 9,
+            0, 10, 2, 2, 10, 12, 4, 14, 6, 6, 14, 8,
+            1, 3, 11, 11, 3, 13, 5, 7, 15, 15, 7, 9).also { whiteOrder ->
+
+            whiteMid = Primitive(whiteMidCords, whiteOrder)
+
             whiteMidCords.copyOf().also { whiteLeftCords ->
-                whiteLeftCords[0] = whiteGap
-                whiteLeftCords[6] = whiteGap
-                whiteLeftCords[30] = whiteGap
-                whiteLeftCords[36] = whiteGap
-                whiteLeft = allocCords(whiteLeftCords)
+                for (i in intArrayOf(0, 6, 30, 36)) whiteLeftCords[i] = whiteGap
+                whiteLeft = Primitive(whiteLeftCords, whiteOrder)
             }
-            whiteMidCords.also { whiteRightCords ->
-                whiteRightCords[3] = whiteWid - whiteGap
-                whiteRightCords[9] = whiteWid - whiteGap
-                whiteRightCords[33] = whiteWid - whiteGap
-                whiteRightCords[39] = whiteWid - whiteGap
-                whiteRight = allocCords(whiteRightCords)
-            }
+            for (i in intArrayOf(3, 9, 33, 39)) whiteMidCords[i] = whiteWid - whiteGap
+            whiteRight = Primitive(whiteMidCords, whiteOrder)
+        } }
+        GLES31.glCreateProgram().also { program ->
+            fun attachShader(type: Int, shaderCode: String) = GLES31.glAttachShader(program,
+                GLES31.glCreateShader(type).also { shader ->
+                    GLES31.glShaderSource(shader, shaderCode)
+                    GLES31.glCompileShader(shader)
+                    if (BuildConfig.DEBUG) GLES31.glGetShaderInfoLog(shader).also { err ->
+                        if (err.isNotEmpty()) throw GLException(0, "Shader compile: $err") }
+                })
+            attachShader(GLES31.GL_VERTEX_SHADER, """
+                attribute vec4 pos;
+                attribute vec3 norm;
+                uniform mat4 mv, mvp;
+                
+                varying vec3 viewDir, normal;
+
+                void main() {
+                    gl_Position = mvp * pos;
+                    viewDir = normalize(vec3(mv * pos));
+                    normal = norm;
+                }""")
+            attachShader(GLES31.GL_FRAGMENT_SHADER, """
+                precision mediump float;
+                uniform vec4 color;
+
+                varying vec3 viewDir, normal;
+                
+                vec3 Diffuse(const vec3 dir, const vec3 diffColor) {
+                    return diffColor * max(-dot(normal, dir), 0.);
+                }
+                vec3 Specular(const vec3 dir, const vec3 specColor) {
+                    return specColor * pow(max(dot(-viewDir, reflect(dir, normal)), 0.), 16.);
+                }
+
+                void main() {
+                    const vec3      lightDir0 = vec3(-.5265408 , -.5735765 , -.6275069),
+                                    lightDir1 = vec3( .7198464 ,  .3420201 ,  .6040227),
+                                    lightDir2 = vec3( .4545195 , -.7660444 ,  .4545195);
+                    gl_FragColor = color * vec4(vec3( .05333332,  .09882354,  .1819608) // ambient
+                          + Diffuse(lightDir0,  vec3(1         ,  .9607844 ,  .8078432))
+                          + Diffuse(lightDir1,  vec3( .9647059 ,  .7607844 ,  .4078432))
+                          + Diffuse(lightDir2,  vec3( .3231373 ,  .3607844 ,  .3937255))
+                         + Specular(lightDir0,  vec3(1         ,  .9607844 ,  .8078432))
+                         + Specular(lightDir1,  vec3(0         , 0         , 0        ))
+                         + Specular(lightDir2,  vec3( .3231373 ,  .3607844 ,  .3937255)), 1);
+                }""")
+            GLES31.glLinkProgram(program)
+            GLES31.glUseProgram(program)
+            GLES31.glEnable(GLES31.GL_CULL_FACE)
+
+            pos = GLES31.glGetAttribLocation(program, "pos")
+            norm = GLES31.glGetAttribLocation(program, "norm")
+            for (handle in intArrayOf(pos, norm)) GLES31.glEnableVertexAttribArray(handle)
+            color = GLES31.glGetUniformLocation(program, "color")
+            mv = GLES31.glGetUniformLocation(program, "mv")
+            mvp = GLES31.glGetUniformLocation(program, "mvp")
         }
-
-        fun attachShader(type: Int, shaderCode: String) = GLES31.glAttachShader(program,
-            GLES31.glCreateShader(type).also { shader ->
-                GLES31.glShaderSource(shader, shaderCode)
-                GLES31.glCompileShader(shader)
-            })
-        attachShader(GLES31.GL_VERTEX_SHADER, """
-            attribute vec4 pos;
-            uniform mat4 mvp;
-            void main() {
-                gl_Position = mvp * pos;
-            }""")
-        attachShader(GLES31.GL_FRAGMENT_SHADER, """
-            precision mediump float;
-            uniform vec4 color;
-            void main() {
-                gl_FragColor = color;
-            }""")
-        GLES31.glLinkProgram(program)
-        GLES31.glUseProgram(program)
-        GLES31.glEnable(GLES31.GL_CULL_FACE)
-
-        pos = GLES31.glGetAttribLocation(program, "pos")
-        color = GLES31.glGetUniformLocation(program, "color")
-        GLES31.glEnableVertexAttribArray(pos)
     }
 
-    fun draw(viewProjection : FloatArray) {
-        GLES31.glGetUniformLocation(program, "mvp").also { mvpHandle -> for (note in 0..87) {
-            fun drawKey(key: FloatBuffer, offset: Float) {
-                GLES31.glVertexAttribPointer(
-                    pos, 3, GLES31.GL_FLOAT, false, 3 * 4, key
-                )
-                GLES31.glUniform4fv(color, 1,
-                    if (key == blackCords) floatArrayOf(.15f, .15f, .15f, 1f)
-                    else floatArrayOf(240 / 255f, 248 / 255f, 255 / 255f, 1f), 0)   // Alice blue
-                FloatArray(16).also {mvpOffset ->
-                    Matrix.translateM(mvpOffset, 0, viewProjection, 0, offset, 0f, 0f)
-                    GLES31.glUniformMatrix4fv(mvpHandle, 1, false, mvpOffset, 0)
-                }
-                GLES31.glDrawElements(
-                    GLES31.GL_TRIANGLES, if (key == blackCords) 24 else 42,
-                    GLES31.GL_UNSIGNED_SHORT, if (key == blackCords) blackOrder else whiteOrder
-                )
-            }
-            when (note) {
-                0 -> drawKey(whiteLeft, 0f)
-                1 -> drawKey(blackCords, 0f)
-                2 -> drawKey(whiteRight, whiteWid)
-                87 -> (((87 - 3) / 12 * 7 + 2) * whiteWid).also { offset ->
-                    drawKey(whiteLeft, offset)
-                    drawKey(whiteRight, offset)
-                }
-                else -> when ((note - 3) % 12) {
-                    0  -> drawKey(whiteLeft,  ((note - 3) / 12 * 7 + 2) * whiteWid)
-                    1  -> drawKey(blackCords, ((note - 3) / 12 * 7 + 2) * whiteWid)
-                    2  -> drawKey(whiteMid,   ((note - 3) / 12 * 7 + 3) * whiteWid)
-                    3  -> drawKey(blackCords, ((note - 3) / 12 * 7 + 3) * whiteWid)
-                    4  -> drawKey(whiteRight, ((note - 3) / 12 * 7 + 4) * whiteWid)
-                    5  -> drawKey(whiteLeft,  ((note - 3) / 12 * 7 + 5) * whiteWid)
-                    6  -> drawKey(blackCords, ((note - 3) / 12 * 7 + 5) * whiteWid)
-                    7  -> drawKey(whiteMid,   ((note - 3) / 12 * 7 + 6) * whiteWid)
-                    8  -> drawKey(blackCords, ((note - 3) / 12 * 7 + 6) * whiteWid)
+    fun draw(view : FloatArray, viewProjection : FloatArray) { for (note in 0..87) {
+        fun drawKey(key : Primitive, offset : Float) {
+            GLES31.glVertexAttribPointer(
+                pos, 3, GLES31.GL_FLOAT, false, 3 * 4, key.vertices)
+            GLES31.glVertexAttribPointer(
+                norm, 3, GLES31.GL_FLOAT, false, 3 * 4, key.normals)
+            GLES31.glUniform4fv(color, 1,
+                if (key == black) floatArrayOf(.15f, .15f, .15f, 1f)
+                else floatArrayOf(240 / 255f, 248 / 255f, 255 / 255f, 1f), 0)   // Alice blue
 
-                    9  -> drawKey(whiteMid,   ((note - 3) / 12 * 7 + 7) * whiteWid)
-                    10 -> drawKey(blackCords, ((note - 3) / 12 * 7 + 7) * whiteWid)
-                    11 -> drawKey(whiteRight, ((note - 3) / 12 * 7 + 8) * whiteWid)
-                }
+            fun translate(matrix : FloatArray, matHandle : Int) { FloatArray(16).also { matOffset ->
+                Matrix.translateM(matOffset, 0, matrix, 0, offset, 0f, 0f)
+                GLES31.glUniformMatrix4fv(matHandle, 1, false, matOffset, 0)
+            } }
+            translate(view, mv)
+            translate(viewProjection, mvp)
+
+            GLES31.glDrawArrays(GLES31.GL_TRIANGLES, 0, key.count)
+        }
+        when (note) {
+            0 -> drawKey(whiteLeft, 0f)
+            1 -> drawKey(black,     0f)
+            2 -> drawKey(whiteRight, whiteWid)
+            87 -> (((87 - 3) / 12 * 7 + 2) * whiteWid).also { offset ->
+                drawKey(whiteLeft, offset)
+                drawKey(whiteRight, offset)
+            }
+            else -> when ((note - 3) % 12) {
+                0  -> drawKey(whiteLeft,  ((note - 3) / 12 * 7 + 2) * whiteWid)
+                1  -> drawKey(black,      ((note - 3) / 12 * 7 + 2) * whiteWid)
+                2  -> drawKey(whiteMid,   ((note - 3) / 12 * 7 + 3) * whiteWid)
+                3  -> drawKey(black,      ((note - 3) / 12 * 7 + 3) * whiteWid)
+                4  -> drawKey(whiteRight, ((note - 3) / 12 * 7 + 4) * whiteWid)
+                5  -> drawKey(whiteLeft,  ((note - 3) / 12 * 7 + 5) * whiteWid)
+                6  -> drawKey(black,      ((note - 3) / 12 * 7 + 5) * whiteWid)
+                7  -> drawKey(whiteMid,   ((note - 3) / 12 * 7 + 6) * whiteWid)
+                8  -> drawKey(black,      ((note - 3) / 12 * 7 + 6) * whiteWid)
+
+                9  -> drawKey(whiteMid,   ((note - 3) / 12 * 7 + 7) * whiteWid)
+                10 -> drawKey(black,      ((note - 3) / 12 * 7 + 7) * whiteWid)
+                11 -> drawKey(whiteRight, ((note - 3) / 12 * 7 + 8) * whiteWid)
             }
         } }
     }
--- app/src/main/java/ru/BShakhovsky/Piano_Transcription/Render.kt
+++ app/src/main/java/ru/BShakhovsky/Piano_Transcription/Render.kt
@@ -25,30 +25,26 @@ class Render : GLSurfaceView.Renderer {
     private val projection     = FloatArray(16)
     private val viewProjection = FloatArray(16)
 
-    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        gl?.let { with (it) {
-            glClearColor(70 / 255f, 130 / 255f, 180 / 255f, 1f)  // Steel blue
-            glEnable(GLES31.GL_DEPTH_TEST)
-        } }
+    override fun onSurfaceCreated(glUnused: GL10?, config: EGLConfig?) {
+        GLES31.glClearColor(70 / 255f, 130 / 255f, 180 / 255f, 1f)  // Steel blue
+        GLES31.glEnable(GLES31.GL_DEPTH_TEST)
         model = Geometry()
     }
-    override fun onSurfaceChanged(gl: GL10?, newWidth: Int, newHeight: Int) {
+    override fun onSurfaceChanged(glUnused: GL10?, newWidth: Int, newHeight: Int) {
         width = newWidth
         height = newHeight
-        gl?.glViewport(0, 0, width, height)
-        (height.toFloat() / width.toFloat()).also { ratio ->
-            Matrix.frustumM(projection, 0, -1f, 1f,
-                -ratio, ratio, 1f, Geometry.overallLen)
-        }
+        GLES31.glViewport(0, 0, width, height)
+        (height.toFloat() / width.toFloat()).also { ratio -> Matrix.frustumM(projection,
+            0, -1f, 1f, -ratio, ratio, 1f, Geometry.overallLen) }
         calcViewProjection()
     }
-    override fun onDrawFrame(gl: GL10?) {
-        gl?.glClear(GLES31.GL_COLOR_BUFFER_BIT or GLES31.GL_DEPTH_BUFFER_BIT)
+    override fun onDrawFrame(glUnused: GL10?) {
+        GLES31.glClear(GLES31.GL_COLOR_BUFFER_BIT or GLES31.GL_DEPTH_BUFFER_BIT)
         if (zoomOut and (SystemClock.uptimeMillis() > zoomTime)) {
             if (zoomTime != 0L) zoom(1 - .0006f * (SystemClock.uptimeMillis() - zoomTime))
             zoomTime = SystemClock.uptimeMillis()
         }
-        model.draw(viewProjection)
+        model.draw(view, viewProjection)
     }
 
     fun move(xStart : Float, xEnd : Float, yStart : Float, yEnd : Float) {
@@ -57,27 +53,25 @@ class Render : GLSurfaceView.Renderer {
 
         fun worldX(xWin : Float, yWin : Float) : Float {
             fun worldCords(zDepth : Float) = floatArrayOf(0f, 0f, 0f, 0f).also { obj ->
-                GLU.gluUnProject(xWin, height - yWin, zDepth,
-                    view, 0, projection, 0,
-                    intArrayOf(0, 0, width, height), 0, obj, 0)
+                GLU.gluUnProject(xWin, height - yWin, zDepth, view, 0, projection,
+                    0, intArrayOf(0, 0, width, height), 0, obj, 0)
                 for (i in 0 .. obj.size - 2) obj[i] /= obj.last()
             }
-            val (xNear, yNear, zNear) = worldCords(0f)
-            val (xFar, yFar, zFar) = worldCords(1f)
-
-            assert((yNear > 0) and (zNear > 0))
-            return if (yStart > height / 2) // (zFar - yFar * (zNear - zFar) / (yNear - yFar) > 0)
-                (xFar - yFar * (xNear - xFar) / (yNear - yFar))
-            else (xFar - zFar * (xNear - xFar) / (zNear - zFar))
+            worldCords(0f).also { (xNear, yNear, zNear) ->
+                worldCords(1f).also { (xFar, yFar, zFar) ->
+                    assert((yNear > 0) and (zNear > 0))
+                    return if (yStart > height / 2) // (zFar - yFar * (zNear - zFar) / (yNear - yFar) > 0)
+                        (xFar - yFar * (xNear - xFar) / (yNear - yFar))
+                    else (xFar - zFar * (xNear - xFar) / (zNear - zFar))
+                }
+            }
         }
         x = 0f.coerceAtLeast(Geometry.overallLen.coerceAtMost(x
                 + worldX(xStart, yStart) - worldX(xEnd, yEnd)))
         calcViewProjection()
     }
     fun zoom(scale : Float) {
-        if (scale < 1) {
-            val xLeft = winX(0f)
-            val xRight = winX(Geometry.overallLen)
+        if (scale < 1) (winX(0f) to winX(Geometry.overallLen)).also { (xLeft, xRight) ->
             if ((xLeft > 0) and (xRight < width)) {
                 zoomOut = false
                 return
@@ -89,17 +83,13 @@ class Render : GLSurfaceView.Renderer {
         calcViewProjection()
     }
 
-    private fun winX(worldX : Float) : Float {
-        floatArrayOf(0f, 0f, 0f).also {
-            GLU.gluProject(worldX, Geometry.whiteWid, Geometry.whiteLen,
-                view, 0, projection, 0,
-                intArrayOf(0, 0, width, height), 0, it, 0)
-            return it[0]
-        }
-    }
+    private fun winX(worldX : Float) : Float { floatArrayOf(0f, 0f, 0f).also { GLU.gluProject(
+        worldX, Geometry.whiteWid, Geometry.whiteLen, view, 0, projection, 0,
+        intArrayOf(0, 0, width, height), 0, it, 0)
+        return it[0]
+    } }
     private fun calcViewProjection() {
-        Matrix.setLookAtM(view, 0, x, yz, yz,
-            x, 0f, 0f, 0f, 1f, 0f)
+        Matrix.setLookAtM(view, 0, x, yz, yz, x, 0f, 0f, 0f, 1f, 0f)
         Matrix.multiplyMM(viewProjection, 0, projection, 0, view, 0)
     }
 }
